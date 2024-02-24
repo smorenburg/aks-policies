95b734d42bb3f8aba00e72b65dc87e83fbfd3362
Robin Smorenburg <robin@robino.io>
2024-02-24 17:46:19 +0100

    Use hex for random

--- scripts/New-StorageAccount.ps1
+++ scripts/New-StorageAccount.ps1
@@ -1,7 +1,7 @@
 $resourceGroupName = "rg-tfstate-astro-neu"
 $location = "northeurope"
 $storageAccountSuffix = "stastro"
-$random = -Join ("0123456789".ToCharArray() | Get-Random -Count 6 | ForEach-Object { [char]$PSItem })
+$random = -Join ("0123456789abcdef".ToCharArray() | Get-Random -Count 4 | ForEach-Object { [char]$PSItem })
 $storageAccountName = $storageAccountSuffix + $random
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location
